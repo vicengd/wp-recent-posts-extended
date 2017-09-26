50d52f7f028ae29ed37763861e599f5f4aa7e0b5
Vicente Garcia <vicengd@gmail.com>
2017-09-27 00:53:22 +0200

    v1.1.2

--- inc/wprpe.php
+++ inc/wprpe.php
@@ -1,22 +1,41 @@
 <?php
-/*
-Plugin Name: WP Recent Posts Extended
-Plugin URI: http://vicentegarcia.com
-Description: A simple widget for displaying recent posts by category
-Author: Vicente García
-Author URI: http://vicentegarcia.com
-Author email: v@vicentegarcia.com
-Version: 1.0
-License: GPLv2
-*/
 
-class wprpe extends WP_Widget
-{
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://vicentegarcia.com
+ * @since      1.0.0
+ *
+ * @package    WP_Recent_Posts_Extended
+ * @subpackage WP_Recent_Posts_Extended/inc
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    WP_Recent_Posts_Extended
+ * @subpackage WP_Recent_Posts_Extended/inc
+ * @author     Vicente Garcia <contacto@vicentegarcia.com>
+ */
+
+
+class wprpe extends WP_Widget {
+
     public function __construct()
     {
         $wprpe_optionss = array('classname' => 'wprpe', 'description' => "Un sencillo widget para mostrar los últimos artículos por categoría." );
         parent::__construct('wprpe','WP Recent Posts Extended', $wprpe_optionss);
-        wp_enqueue_style( $this->widgetName.'-styles', plugins_url( 'wprpewidget.css', __FILE__ ));
+        wp_enqueue_style( 'wprpe-styles', plugins_url( 'wprpewidget.css', __FILE__ ));
     }
 
     function widget($args,$instance)
@@ -42,16 +61,23 @@ class wprpe extends WP_Widget
     function update($new_instance, $old_instance)
     {
         $instance = $old_instance;
-        $instance["wprpe_categories_exclude"] = strip_tags($new_instance["wprpe_categories_exclude"]);
+        $instance["wprpe_categories_exclude"] = ( ! empty( $new_instance['wprpe_categories_exclude'] ) ) ? strip_tags($new_instance["wprpe_categories_exclude"]) : '';
         return $instance;
     }
 
     function form($instance)
     {
+        $categories_exclude = empty ($instance['wprpe_categories_exclude']) ? '' : $instance['wprpe_categories_exclude'];
         ?>
          <p>
-            <label for="<?php echo $this->get_field_id('wprpe_categories_exclude'); ?>">Categorías a excluir (separadas por comas)</label>
-            <input class="widefat" id="<?php echo $this->get_field_id('wprpe_categories_exclude'); ?>" name="<?php echo $this->get_field_name('wprpe_categories_exclude'); ?>" type="text" value="<?php echo esc_attr($instance["wprpe_categories_exclude"]); ?>" />
+            <label for="<?php echo esc_attr ($this->get_field_id('wprpe_categories_exclude')); ?>">
+            <?php esc_attr_e( 'Categorías a excluir (separadas por comas): ', 'text_domain' ); ?>
+            </label>
+            <input
+            class="widefat"
+            id="<?php echo esc_attr ($this->get_field_id('wprpe_categories_exclude')); ?>"
+            name="<?php echo esc_attr ($this->get_field_name('wprpe_categories_exclude')); ?>"
+            type="text" value="<?php echo $categories_exclude; ?>">
         </p>
          <?php
     }
@@ -76,11 +102,14 @@ class wprpe extends WP_Widget
         $stringwprpe .= "</ul>\n";
 
         return $stringwprpe;
+
     }
 
     public function category_get_posts($numPosts = '0',$category = '')
     {
         global $wpdb, $wp_db_version;
+
+        $notfirst = false;
 
         if($category == ''):
             $sql = "SELECT $wpdb->posts.ID FROM $wpdb->posts WHERE $wpdb->posts.post_status = 'publish' AND $wpdb->posts.post_type = 'post'";
@@ -133,7 +162,8 @@ class wprpe extends WP_Widget
         {
             foreach ($posts_by_category as $item)
             {
-                $posts_results[] = array('title'=>str_replace('"','',stripslashes($item->post_title)),'permalink'=>post_permalink($item->ID));
+                // $posts_results[] = array('title'=>str_replace('"','',stripslashes($item->post_title)),'permalink'=>post_permalink($item->ID));
+                $posts_results[] = array('title'=>str_replace('"','',stripslashes($item->post_title)), 'permalink'=>get_permalink($item->ID));
             }
             return $posts_results;
         }
--- wp-recent-posts-extended.php
+++ wp-recent-posts-extended.php
@@ -1,20 +1,37 @@
 <?php
-/*
-Plugin Name: WP Recent Posts Extended
-Plugin URI: http://vicentegarcia.com
-Description: A simple widget for displaying recent posts by category
-Author: Vicente García
-Author URI: http://vicentegarcia.com
-Author email: v@vicentegarcia.com
-Version: 1.0
-License: GPLv2
-*/
+
+/**
+ * WP Recent Posts Extended
+ *
+ * @link              https://vicentegarcia.com
+ * @since             1.0.0
+ * @package           WP_Recent_Posts_Extended
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Recent Posts Extended
+ * Plugin URI:        https://wordpress.org/plugins/wp-recent-posts-extended/
+ * Description:       A simple widget for displaying recent posts by category.
+ * Version:           1.1.2
+ * Author:            Vicente Garcia
+ * Author URI:        https://vicentegarcia.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-recent-posts-extended
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
 /**
  * Function to register widget
  */
+
 function wprpe_create_widget(){
     include_once(plugin_dir_path( __FILE__ ).'/inc/wprpe.php');
     register_widget('wprpe');
 }
 add_action('widgets_init','wprpe_create_widget');
+
